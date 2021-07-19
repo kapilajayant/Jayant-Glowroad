c8ad17fab037ee1f859dd65d9cbd6562f8736d45
kapilajayant <kapilajayant@gmail.com>
2021-07-20 00:56:57 +0530

    Recycler View setup

---
+++ app/src/main/java/com/jayant/glowroadjayant/adapters/PhotosAdapter.kt
@@ -0,0 +1,50 @@
+package com.jayant.glowroadjayant.adapters
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.Target
+import com.jayant.glowroadjayant.R
+import com.jayant.glowroadjayant.models.PhotoModel
+
+class PhotosAdapter(var context: Context, var photosList: ArrayList<PhotoModel>): RecyclerView.Adapter<PhotosViewHolder>() {
+    private val TAG = PhotosAdapter::class.java.simpleName
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotosViewHolder {
+        val view: View = LayoutInflater.from(context).inflate(
+            R.layout.layout_row_item,
+            parent,
+            false
+        )
+        return PhotosViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PhotosViewHolder, position: Int) {
+
+        Log.d(TAG, "onBindViewHolder title: ${photosList.get(position).title}")
+        Log.d(TAG, "onBindViewHolder url: ${photosList.get(position).url_q}")
+
+        holder.tv.text = photosList.get(position).title
+
+        Glide.with(context)
+            .load(photosList.get(position).url_q)
+            .centerCrop()
+            .into(holder.iv)
+    }
+
+    override fun getItemCount(): Int {
+        return photosList.size
+    }
+}
+
+class PhotosViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    val tv = itemView.findViewById<TextView>(R.id.tv)
+    val iv = itemView.findViewById<ImageView>(R.id.iv)
+}
--- app/src/main/java/com/jayant/glowroadjayant/ui/MainActivity.kt
+++ app/src/main/java/com/jayant/glowroadjayant/ui/MainActivity.kt
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.jayant.glowroadjayant.adapters.PhotosAdapter
 import com.jayant.glowroadjayant.databinding.ActivityMainBinding
 import com.jayant.glowroadjayant.models.ApiResponse
+import com.jayant.glowroadjayant.models.PhotoModel
 import com.jayant.glowroadjayant.network.ApiUtils
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -20,6 +24,10 @@ class MainActivity : AppCompatActivity() {
     private val TAG = MainActivity::class.java.simpleName
     lateinit var binding: ActivityMainBinding
 
+    private lateinit var photosList: ArrayList<PhotoModel>
+
+    private var adapter: PhotosAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -28,13 +36,19 @@ class MainActivity : AppCompatActivity() {
 
         binding.shimmerFrameLayout.startShimmerAnimation()
 
+        photosList = ArrayList()
+
         setupUI()
+
         getPhotos("1")
 
     }
 
     private fun setupUI() {
 
+        val linearLayoutManager = LinearLayoutManager(this)
+        linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
+        binding.rvImages.layoutManager = linearLayoutManager
     }
 
     private fun getPhotos(page: String) {
@@ -43,11 +57,23 @@ class MainActivity : AppCompatActivity() {
             ApiUtils.getApiService()?.getPhotos(page)?.enqueue(object : Callback<ApiResponse> {
                 override fun onResponse(call: Call<ApiResponse>, response: Response<ApiResponse>) {
                     binding.shimmerFrameLayout.stopShimmerAnimation()
-                    binding.shimmerFrameLayout.visibility  = View.GONE
+                    binding.shimmerFrameLayout.visibility = View.GONE
                     if (response.isSuccessful) {
                         try {
                             Log.d(TAG, "onResponse: ${response.body()}")
                             Log.d(TAG, "onResponse: ${response.body()?.photos?.photo?.size}")
+
+                            response.body()?.photos?.photo?.forEach { photoModel ->
+                                photosList.add(photoModel)
+                            }
+
+                            Log.d(TAG, "onResponse: $photosList")
+
+                            adapter = PhotosAdapter(this@MainActivity, photosList)
+                            binding.rvImages.adapter = adapter
+                            binding.rvImages.visibility = View.VISIBLE
+                            adapter?.notifyDataSetChanged()
+
                         } catch (e: Exception) {
                             e.printStackTrace()
                         }
@@ -57,7 +83,7 @@ class MainActivity : AppCompatActivity() {
                 override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
                     Log.d(TAG, "onFailure: ${t.message}")
                     binding.shimmerFrameLayout.stopShimmerAnimation()
-                    binding.shimmerFrameLayout.visibility  = View.GONE
+                    binding.shimmerFrameLayout.visibility = View.GONE
 
                 }
 
