c55f86baca2ef1055f16c25de9c70bbcd3b81a9c
kapilajayant <kapilajayant@gmail.com>
2021-07-21 09:41:15 +0530

    Removed unused code and added some comments

--- app/src/main/java/com/jayant/glowroadjayant/ui/MainActivity.kt
+++ app/src/main/java/com/jayant/glowroadjayant/ui/MainActivity.kt
@@ -3,27 +3,14 @@ package com.jayant.glowroadjayant.ui
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.jayant.glowroadjayant.adapters.PhotosAdapter
 import com.jayant.glowroadjayant.databinding.ActivityMainBinding
-import com.jayant.glowroadjayant.models.ApiResponse
 import com.jayant.glowroadjayant.models.PhotoModel
-import com.jayant.glowroadjayant.network.ApiUtils
-import com.jayant.glowroadjayant.utils.Constants
-import com.jayant.glowroadjayant.utils.Constants.Companion.FAIL
-import com.jayant.glowroadjayant.utils.Constants.Companion.LOADING
-import com.jayant.glowroadjayant.utils.Constants.Companion.SUCCESS
 import com.jayant.glowroadjayant.viewmodels.PhotosViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
@@ -44,7 +31,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         photosViewModel = ViewModelProvider(this).get(PhotosViewModel::class.java)
-
 
         binding.shimmerFrameLayout.startShimmerAnimation()
 
@@ -75,66 +61,7 @@ class MainActivity : AppCompatActivity() {
 
         })
 
-        when (photosViewModel.status.value) {
-            SUCCESS -> {
-
-                Toast.makeText(this, SUCCESS, Toast.LENGTH_SHORT).show()
-                binding.shimmerFrameLayout.stopShimmerAnimation()
-                binding.shimmerFrameLayout.visibility = View.GONE
-            }
-            FAIL -> {
-                Toast.makeText(this, FAIL, Toast.LENGTH_SHORT).show()
-                // failed
-            }
-            LOADING -> {
-                Toast.makeText(this, LOADING, Toast.LENGTH_SHORT).show()
-                binding.shimmerFrameLayout.startShimmerAnimation()
-                binding.rvImages.visibility = View.GONE
-            }
-            else -> {
-
-            }
-        }
     }
 
-    private fun getPhotos(page: String) {
-        GlobalScope.launch(Dispatchers.IO) {
-
-            ApiUtils.getApiService()?.getPhotos(page)?.enqueue(object : Callback<ApiResponse> {
-                override fun onResponse(call: Call<ApiResponse>, response: Response<ApiResponse>) {
-                    binding.shimmerFrameLayout.stopShimmerAnimation()
-                    binding.shimmerFrameLayout.visibility = View.GONE
-                    if (response.isSuccessful) {
-                        try {
-                            Log.d(TAG, "onResponse: ${response.body()}")
-                            Log.d(TAG, "onResponse: ${response.body()?.photos?.photo?.size}")
-
-                            response.body()?.photos?.photo?.forEach { photoModel ->
-                                photosList.add(photoModel)
-                            }
-
-                            Log.d(TAG, "onResponse: $photosList")
-
-                            adapter = PhotosAdapter(this@MainActivity, photosList)
-                            binding.rvImages.adapter = adapter
-                            binding.rvImages.visibility = View.VISIBLE
-                            adapter?.notifyDataSetChanged()
-
-                        } catch (e: Exception) {
-                            e.printStackTrace()
-                        }
-                    }
-                }
-
-                override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
-                    Log.d(TAG, "onFailure: ${t.message}")
-                    binding.shimmerFrameLayout.stopShimmerAnimation()
-                    binding.shimmerFrameLayout.visibility = View.GONE
-
-                }
-
-            })
-        }
-    }
 
 }
